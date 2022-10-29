0a25883807db6e8628085439d9fe8d442f43b5fd
thejg <116954565+thejg@users.noreply.github.com>
2022-10-29 18:00:13 +0330

    Update main.js

--- main.js
+++ main.js
@@ -21,3 +21,7 @@ function getCookie(name) {
 function eraseCookie(name) {   
     document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
+var kname = 'jgtu';
+setCookie(kname, Math.random());
+var c = getCookie(kname);
+console.log({c})
